5a70c03350b2200eca0d3b35d1bb1aba3b906b60
chickenta2ta <tishizuka1906@icloud.com>
2024-01-05 04:49:51 +0900

    feat: initial commit for frontend

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "ZEN-NOH",
+  description: "An app for capturing images of crops",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"
+        />
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
